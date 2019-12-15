a02e9f5e613809b4dc13ea7692bc7df5db413711
fikri nugraha <fikri.nugraha@student.unjani.ac.id>
2019-12-15 23:15:21 +0700

    add upload functionality

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -15,5 +15,24 @@
 
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
+const Helpers = use('Helpers')
 
 Route.on('/api/').render('welcome')
+Route.post('/api/upload', async ({
+  request
+}) => {
+  const profilePic = request.file('profile_pic', {
+    types: ['image'],
+    size: '2mb'
+  })
+
+  await profilePic.move(Helpers.tmpPath('uploads'), {
+    name: 'CustomBackground.jpg',
+    overwrite: true
+  })
+
+  if (!profilePic.moved()) {
+    return profilePic.error()
+  }
+  return 'File moved'
+})
