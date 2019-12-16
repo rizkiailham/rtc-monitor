0e85758786f9acf1197ae1b73085fcd00faf491b
fikri nugraha <fikri.nugraha@student.unjani.ac.id>
2019-12-17 02:12:08 +0700

    fix url

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -11,7 +11,6 @@ Route.post('/api/upload/', async ({ request }) => {
       types: ['image'],
       size: '24mb'
     })
-
     await backgroundPic.move(Helpers.publicPath('uploads'), {
       name: 'CustomBackground.jpg',
       overwrite: true
@@ -20,7 +19,7 @@ Route.post('/api/upload/', async ({ request }) => {
     if (!backgroundPic.moved()) {
       return backgroundPic.error()
     }
-    return "http://localhost:3333/uploads/CustomBackground.jpg?t="+ Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) ;
+    return "https://rtc.ajak.in/uploads/CustomBackground.jpg?t="+ Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10) ;
   }catch(e){
     console.log(e)
   }
